713bdb8083a732cfd1aec3ba400761cefb9413bd
Coeur <coeur@ceremonyofsin.org>
2024-12-05 14:03:35 -0600

    Re-initialise environment to use Vue and Express

--- android/app/src/main/kotlin/com/archieBell/web/web/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.archieBell.web.web
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ client/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="p-10 grid content-center place-items-center h-screen">
+    <h1 class="font-bold uppercase text-3xl">Missing Persons Form</h1>
+
+    <div class="pt-5 grid grid-cols-12 content-center place-items-center">
+
+      <div class="col-span-6 mr-1">
+        <label for="name" class="mr-2 block content-center">Name</label>
+        <input type="text" name="name" :bind="name" id="name" placeholder="John Doe" class="border rounded-lg p-1">   
+
+        <label for="age" class="mr-2 block content-center pt-2">Age</label>
+        <input type="text" name="age" :bind="age" id="age" placeholder="18" class="border rounded-lg p-1">   
+      </div>
+
+      <div class="col-span-6 ml-1">
+        <label for="name" class="mr-2 block content-center">Last Location Seen</label>
+        <input type="text" name="lastLocationSeen" :bind="lastLocationSeen" id="lastLocationSeen" placeholder="Moose Jaw, SK" class="border rounded-lg p-1">
+
+        <label for="name" class="mr-2 block content-center pt-2">Last Date & Time Seen</label>
+        <input type="text" name="lastDateTimeSeen" :bind="lastDateTimeSeen" id="lastDateTimeSeen" placeholder="12:00 PM" class="border rounded-lg p-1">
+      </div>
+
+    </div>
+    <div class="pt-2">
+        <label for="additonalInfo" class="mr-2 block content-center">Additional Info</label>
+        <textarea name="additionalInfo" id="additionalInfo" cols="45" rows="5" wrap="hard" maxlength="200" class="border rounded-lg p-1 resize-none" placeholder="Add extra information about this person here."></textarea>
+    </div>
+
+  </div>
+</template>
